5c7c118fcb6a3444d82d6aad8419f71dd62c466f
matthew theseira <matt_.t993@hotmail.com>
2024-02-09 11:58:06 +1100

    get method for image banner working

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,30 +1,46 @@
 import { baseUrl } from "@/constants/movie";
 import { Movie } from "@/typing";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import React, { useEffect, useState } from "react";
 
-interface Props{
-  netflixOriginals: Movie[]
+interface Props {
+  netflixOriginals: Movie[];
 }
 
-const Banner = ({netflixOriginals}:Props) => {
-  const [movie, setMovie] = useState<Movie | null>(null)
+const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null);
 
-  useEffect(() =>{
-    setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
-  },[netflixOriginals])
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
 
   console.log(movie);
   return (
-    <div>
-      <div className="absolute top-0 left-0 h-[95vh] w-screen">
-        <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`} 
-        alt=""
-        layout="fill"
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end
+    lg:pb-12">
+      <div className="absolute top-0 left-0 h-[95vh] -z-10 w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          alt=""
+          layout="fill"
+          objectFit="cover"
         />
       </div>
+      <h1 className="text-2xl lg:text-7xl md:text-4xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+
+      <div>
+        <button className="banner__button">Play</button>
+        <button className="banner__button">More Info</button>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Banner;
